d16bdadae0f46fe299219cdbb32dd826d3234bb5
Lê Huy <huy110903@gmail.com>
2024-07-11 12:03:32 +0700

    update fix

--- Assets/Scripts/Management/GameManager.cs
+++ Assets/Scripts/Management/GameManager.cs
@@ -1,4 +1,7 @@
 using Assets.Scripts.Enums;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -12,6 +15,10 @@ public class GameManager : Singleton<GameManager>
     [Header("UI")]
     [SerializeField]
     private GameObject pauseScreen;
+
+    // Reference to the game object you want to save
+    [SerializeField]
+    private GameObject gameObjectToSave;
 
     private new void Awake()
     {
@@ -67,6 +74,54 @@ public class GameManager : Singleton<GameManager>
             {
                 PauseGame();
             }
+        }
+    }
+
+    // Function to save the game
+    public void SaveGame()
+    {
+        // Create a binary formatter to serialize the object
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        // Create a file to save the serialized data
+        string savePath = Application.persistentDataPath + "/savegame.dat";
+        FileStream fileStream = File.Create(savePath);
+
+        // Serialize the game object and write it to the file
+        formatter.Serialize(fileStream, gameObjectToSave);
+
+        // Close the file stream
+        fileStream.Close();
+
+        Debug.Log("Game saved!");
+    }
+
+    // Function to load the saved game
+    public void LoadGame()
+    {
+        // Check if the save file exists
+        string savePath = Application.persistentDataPath + "/savegame.dat";
+        if (File.Exists(savePath))
+        {
+            // Create a binary formatter to deserialize the object
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            // Open the save file
+            FileStream fileStream = File.Open(savePath, FileMode.Open);
+
+            // Deserialize the game object from the file
+            GameObject loadedGameObject = (GameObject)formatter.Deserialize(fileStream);
+
+            // Close the file stream
+            fileStream.Close();
+
+            // Instantiate the loaded game object in the scene
+            Instantiate(loadedGameObject);
+
+            Debug.Log("Game loaded!");
+        } else
+        {
+            Debug.Log("No save file found!");
         }
     }
 
