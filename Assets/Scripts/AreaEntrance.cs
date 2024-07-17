0a7ffd46fffef080db43b7078e4b599805b0fc20
Nguyen Tien Toan <143169549+tientoan@users.noreply.github.com>
2024-07-17 17:37:15 +0700

    fix scene

--- Assets/Scripts/AreaEntrance.cs
+++ Assets/Scripts/AreaEntrance.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AreaEntrance : MonoBehaviour
 {
     [SerializeField] private string transitionName;
-
+    
     private void Start()
     {
         if (transitionName == SceneManagement.Instance.SceneTransitionName)
